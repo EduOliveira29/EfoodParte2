e720e9230ac113176ac1adca496e0a1000f4081b
EduOliveira29 <realmadri123@yahoo.com.br>
2025-08-08 19:36:12 -0300

    atualizando a apresentação com a nova api

--- src/Componentes/Apresentacao/index.tsx
+++ src/Componentes/Apresentacao/index.tsx
@@ -1,26 +1,21 @@
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { Restaurante } from '../../pages/Perfil'
 import { Container, Imagem } from './styles'
+import { useGetRestaurantesQuery } from '../../services/api'
 
 const Apresentacao = () => {
-  const { id } = useParams()
-  const [Restaurante, setRestaurante] = useState<Restaurante>()
+  const { data: restaurantes } = useGetRestaurantesQuery()
 
-  useEffect(() => {
-    fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`)
-      .then((res) => res.json())
-      .then((res) => setRestaurante(res))
-  }, [id])
-  return (
-    <>
-      <Imagem style={{ backgroundImage: `url(${Restaurante?.capa})` }}>
-        <Container>
-          <h2>{Restaurante?.tipo}</h2>
-          <h3>{Restaurante?.titulo}</h3>
-        </Container>
-      </Imagem>
-    </>
-  )
+  if (restaurantes) {
+    return (
+      <>
+        <Imagem style={{ backgroundImage: `url(${restaurantes[0].capa})` }}>
+          <Container>
+            <h2>{restaurantes[0].tipo}</h2>
+            <h3>{restaurantes[0].titulo}</h3>
+          </Container>
+        </Imagem>
+      </>
+    )
+  }
+  return <h4>Pagina carregando...</h4>
 }
 export default Apresentacao
