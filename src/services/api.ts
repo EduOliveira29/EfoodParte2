87b9d8a5766be0d09e233c9a34a1bfdb757ea3cf
EduOliveira29 <realmadri123@yahoo.com.br>
2026-05-23 12:16:20 -0300

    atualizando a api, e recolocando as imgs

--- src/services/api.ts
+++ src/services/api.ts
@@ -33,7 +33,7 @@ export type ComprarCartao = {
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://ebac-fake-api.vercel.app/api/efood'
+    baseUrl: 'https://api-ebac.vercel.app/api/efood'
   }),
   endpoints: (builder) => ({
     getRestaurantes: builder.query<Restaurantes[], void>({
